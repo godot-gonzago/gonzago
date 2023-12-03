c7c12f4ce3ba54b92a7e3aad5f0c437614adb92a
David Krummenacher <dave@kruda.ch>
2023-12-03 15:55:47 +0100

    Minor changes.

--- addons/gonzago/core/engine.gd
+++ addons/gonzago/core/engine.gd
@@ -1,6 +1,11 @@
 @tool
 class_name GonzagoEngine
 extends Node
-
-
 ## Gonzago autoload singleton
+
+## The general group this node gets added to.
+const GONZAGO_ENGINE_GROUP := &"gonzago.engine"
+
+
+func _ready() -> void:
+    add_to_group(GONZAGO_ENGINE_GROUP, true)
--- addons/gonzago/core/system.gd
+++ addons/gonzago/core/system.gd
@@ -1,11 +1,17 @@
 @tool
 class_name GonzagoSystem
 extends Node
-
-
 ## Base class for Gonzago systems.
+##
 ## Systems can be loaded or unloaded based on
 ## application state or feature flags.
 ## Systems are kept alive as long as the
 ## instantiating application state or a substate
 ## thereof is active.
+
+## The general system group this node gets added to.
+const GONZAGO_SYSTEM_GROUP := &"gonzago.system"
+
+
+func _ready() -> void:
+    add_to_group(GONZAGO_SYSTEM_GROUP, true)
