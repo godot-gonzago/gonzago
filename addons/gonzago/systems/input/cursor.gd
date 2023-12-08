8e33196a6affdbe466dd2d4821e3c9d78e954f0c
David Krummenacher <dave@kruda.ch>
2023-12-08 23:43:19 +0100

    Merp.

--- addons/gonzago/systems/input/cursor.gd
+++ addons/gonzago/systems/input/cursor.gd
@@ -1,3 +1,21 @@
 @tool
 class_name GonzagoCursor
 extends CanvasLayer
+
+
+@export var position: Node2D
+
+
+func _ready() -> void:
+    if Engine.is_editor_hint():
+        return
+
+    position.position = position.get_global_mouse_position()
+
+
+func _input(event: InputEvent) -> void:
+    if Engine.is_editor_hint():
+        return
+
+    if event is InputEventMouseMotion:
+        position.position = event.position
