2b6b52c939ced57bd3f41e649c8161bcfcba60d3
David Krummenacher <dave@kruda.ch>
2023-12-23 16:25:12 +0100

    Merp.

--- addons/gonzago/editor/quickbar/quickbar.gd
+++ addons/gonzago/editor/quickbar/quickbar.gd
@@ -3,6 +3,24 @@ class_name GonzagoEditorQuickbar
 extends PanelContainer
 
 
+var _items: Control
+
+
+func _ready() -> void:
+    _items = get_node("Items") as Control
+
+
+func _notification(what: int) -> void:
+    match what:
+        NOTIFICATION_SORT_CHILDREN:
+            var has_visible_children := false
+            for child in _items.get_children():
+                if child is Control and child.visible:
+                    has_visible_children = true
+                    break
+            visible = has_visible_children
+
+
 func add_icon_button() -> Button:
     var button := Button.new()
     button.flat = true
