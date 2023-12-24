eddb0cb4c6a903f938dd77386e193d78f91e6abf
David Krummenacher <dave@kruda.ch>
2023-12-24 20:59:11 +0100

    Merp.

--- addons/gonzago-dev_tools/theme_explorer/explorer.gd
+++ addons/gonzago-dev_tools/theme_explorer/explorer.gd
@@ -1,2 +1,8 @@
 @tool
 extends VBoxContainer
+
+
+func _draw() -> void:
+    var rect := Rect2(Vector2.ZERO, size)
+    var bg := get_theme_stylebox("BottomPanelDebuggerOverride", "EditorStyles")
+    draw_style_box(bg, rect)
