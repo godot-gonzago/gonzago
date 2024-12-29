4317179b9e92e88bb2f9d7dfe1fe40b8696e05a8
David Krummenacher <dave@kruda.ch>
2024-12-29 23:39:23 +0100

    Merp.

--- addons/gonzago/core/app.gd
+++ addons/gonzago/core/app.gd
@@ -6,6 +6,18 @@ extends Node
 ## Main entry point of the application.
 
 
+# MainAppStatess:
+# - Transitional
+# - Splash? Initialization?
+# - MainMenu
+#   - StartScreen? Input detection
+#   - Submenus (Main, Load, Settings, etc.)
+# - Gameplay
+#   - Levels (Gameplay, GameplayMenus)
+#   - PauseMenu (not while GameplayMenu?)
+#     - Submenus (Load, Settings)
+# - Credits (auto-transition back to MainMenu)
+#
 # AppStatesGraph
 # +--------------+
 # |  BootSplash  | Initialize
