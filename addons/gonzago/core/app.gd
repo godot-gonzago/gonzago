7e2c1f7fa94b5345d9f98b311bb468e2bfc36cfc
David Krummenacher <dave@kruda.ch>
2024-12-29 23:30:30 +0100

    Merp.

--- addons/gonzago/core/app.gd
+++ addons/gonzago/core/app.gd
@@ -4,3 +4,27 @@ extends Node
 
 
 ## Main entry point of the application.
+
+
+# AppStatesGraph
+# +--------------+
+# |  BootSplash  | Initialize
+# +--------------+
+# +--------------+
+# | StartScreen  | Input gathering
+# +--------------+
+# +--------------++--------------+
+# |   MainMenu   ||  SubMenu     | No transitions horizontally? Live in same scene
+# +--------------++--------------+
+# +--------------+
+# |GameplayState |
+# +--------------+
+#      +--------------++-------------++--------------+
+#      | Level01      ||  Level02    || GameplayMenu | Loading Ssreen On Level Load
+#      +--------------++-------------++--------------+
+#      +--------------++--------------++--------------+
+#      |  PauseMenu   ||   SubMenu    ||   SubMenu    |
+#      +--------------++--------------++--------------+
+# +--------------+
+# |   Credits    |
+# +--------------+
