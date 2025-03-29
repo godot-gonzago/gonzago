55f64a962d69a94d971351e39d4b215d676aab57
David Krummenacher <dave@kruda.ch>
2025-03-29 01:10:20 +0100

    Merp.

--- addons/gonzago/core/gonzago.gd
+++ addons/gonzago/core/gonzago.gd
@@ -2,7 +2,7 @@
 @static_unload
 class_name Gonzago
 extends RefCounted
-## Gonzago.
+## Gonzago utilities.
 ##
 ## TODO: Document according to
 ##       https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_documentation_comments.html
