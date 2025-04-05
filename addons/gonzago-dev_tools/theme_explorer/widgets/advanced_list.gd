37c527498859fa5019e1dfdcd8f27ff8aa9c0527
David Krummenacher <dave@kruda.ch>
2025-04-05 17:16:14 +0200

    Merp.

---
+++ addons/gonzago-dev_tools/theme_explorer/widgets/advanced_list.gd
@@ -0,0 +1,17 @@
+@tool
+extends Container
+
+# https://github.com/godotengine/godot/blob/master/editor/editor_inspector.h
+# https://github.com/godotengine/godot/blob/master/editor/editor_inspector.cpp
+# https://github.com/SirLich/gd-explorer
+
+class Group extends Container:
+    # TODO: Collapsable group like tree or inspector
+    pass
+
+class Item extends Container:
+    # TODO: Regular view: 2 Columns like inspector
+    #       Editable label, Buttons after the label (hidden in thumbnail mode)
+    #       Control on the right side
+    #       Thumbnail drawing registerable callback in thumbnail mode
+    pass
