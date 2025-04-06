59e05bb275261b64aa6e1e4a50060fe55be60f90
David Krummenacher <dave@kruda.ch>
2025-04-06 19:31:42 +0200

    Merp.

--- addons/gonzago-dev_tools/theme_explorer/widgets/advanced_list.gd
+++ addons/gonzago-dev_tools/theme_explorer/widgets/advanced_list.gd
@@ -6,16 +6,39 @@ extends Container
 # https://github.com/SirLich/gd-explorer
 # https://github.com/wareya/ScrollListContainer/tree/main
 
-class Group extends Container:
-    # TODO: Collapsable group like tree or inspector
-    pass
+enum DisplayMode {
+    DISPLAY_MODE_LIST,
+    DISPLAY_MODE_THUMBNAIL
+}
 
-class Item extends Container:
+class ListElementButton extends Object:
+    var disabled: bool = true
+    var visible: bool = true
+    var icon: Texture2D
+
+class ListElement extends Object:
+    var selectable: bool = true
+    var disabled: bool = true
+    var visible: bool = true
+    var icon: Texture2D
+    var text: String
+    var editable_text: bool = false
+    var tooltip: String
+    var tooltip_enabled: bool
+    var buttons: Array[ListElementButton]
+    var metadata: Variant
+
+class ListGroup extends ListElement:
+    # TODO: Collapsable group like tree or inspector
+    var folded: bool
+
+class ListItem extends ListElement:
     # TODO: Regular view: 2 Columns like inspector
     #       Editable label, Buttons after the label (hidden in thumbnail mode)
     #       Control on the right side
     #       Thumbnail drawing registerable callback in thumbnail mode
-    pass
+    var custom_control: Control
+    var custom_preview_callback: Callable
 
 
 func _init() -> void:
