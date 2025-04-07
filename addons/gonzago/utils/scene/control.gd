b4b791ed65b64a2c4211226e1739d141cf657c72
David Krummenacher <dave@kruda.ch>
2025-04-07 13:04:13 +0200

    Custom control theming is impossible.

--- addons/gonzago/utils/scene/control.gd
+++ addons/gonzago/utils/scene/control.gd
@@ -3,5 +3,16 @@
 extends RefCounted
 
 # TODO: Get children for container.
-# TODO: Theming and orverrides handling
+
+#region Themeing
+
+# TODO: Theming and overrides handling
 #       https://github.com/kenyoni-software/godot-addons
+#       https://docs.godotengine.org/en/stable/classes/class_control.html#class-control-method-get-theme-color
+#       https://docs.godotengine.org/en/stable/classes/class_control.html#class-control-method-has-theme-color
+#       https://docs.godotengine.org/en/stable/classes/class_control.html#class-control-method-has-theme-color-override
+#       https://docs.godotengine.org/en/stable/classes/class_control.html#class-control-property-theme
+#       https://docs.godotengine.org/en/stable/classes/class_control.html#class-control-property-theme-type-variation
+#       https://docs.godotengine.org/en/stable/classes/class_control.html#class-control-method-get-parent-control
+
+#endregion
