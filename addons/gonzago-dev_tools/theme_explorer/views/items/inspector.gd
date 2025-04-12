b3dc9c81dde195532eba471858d7ab866522ecbd
David Krummenacher <dave@kruda.ch>
2025-04-12 21:17:32 +0200

    Added some inspector stuff.

--- addons/gonzago-dev_tools/theme_explorer/views/items/inspector.gd
+++ addons/gonzago-dev_tools/theme_explorer/views/items/inspector.gd
@@ -14,6 +14,7 @@ const NodeUtil := Gonzago.NodeUtil
 const ThemeUtil := Gonzago.ThemeUtil
 
 @onready var _hierarchy_button := get_node("Header/HierarchyButton") as OptionButton
+@onready var _editor := get_node("Editor") as Control
 @onready var _preview_box := get_node("PreviewBox") as PanelContainer
 @onready var _preview := get_node("PreviewBox/Preview") as Control
 
@@ -23,11 +24,30 @@ var _data_type: Theme.DataType = Theme.DATA_TYPE_MAX
 var _theme_type: StringName = StringName()
 var _theme_item: StringName = StringName()
 
+var _color_picker := ColorPickerButton.new()
+var _spin_box := SpinBox.new()
+var _resource_picker := EditorResourcePicker.new()
+
+
+func _init() -> void:
+    _color_picker.visible = false
+    _color_picker.text = "Color"
+    _spin_box.visible = false
+    _spin_box.rounded = true
+    _spin_box.min_value = 0.0
+    _spin_box.max_value = 100.0
+    _spin_box.allow_greater = true
+    _resource_picker.visible = false
+
 
 func _notification(what: int) -> void:
     match what:
         NOTIFICATION_READY:
             if not NodeUtil.is_node_being_edited(self):
+                _editor.add_child(_color_picker)
+                _editor.add_child(_spin_box)
+                _editor.add_child(_resource_picker)
+                
                 _preview.draw.connect(_draw_preview)
                 _update_inspector()
 
@@ -77,6 +97,14 @@ func inspect_theme_item(
 
 func _update_inspector() -> void:
     _hierarchy_button.clear()
+    
+    _color_picker.visible = false
+    _spin_box.visible = false
+    _resource_picker.visible = false
+    _resource_picker.edited_resource = null
+    _resource_picker.base_type = ""
+    _preview_box.visible = false
+    
     for mode in range(_mode, -1, -1):
         var idx := _hierarchy_button.item_count
         _hierarchy_button.add_item("")
@@ -101,10 +129,48 @@ func _update_inspector() -> void:
                 var icon := ThemeUtil.get_data_type_icon(_data_type)
                 _hierarchy_button.set_item_icon(idx, icon)
                 _hierarchy_button.set_item_text(idx, _theme_item)
+                _preview_box.visible = true
+                _preview.queue_redraw()
+                _update_theme_item_inspector()
                 
-    _preview_box.visible = _mode == Mode.THEME_ITEM
-    _preview.queue_redraw()
+                
+func _update_theme_item_inspector() -> void:
+    var value := ThemeUtil.get_theme_item(_theme, _data_type, _theme_type, _theme_item)
+    # TODO: Handle editable
+    match _data_type:
+        Theme.DATA_TYPE_COLOR:
+            var color: Color = value as Color
+            _color_picker.visible = true
+            _color_picker.color = color
+        Theme.DATA_TYPE_CONSTANT:
+            var constant: int = value as int
+            var constant_type := ThemeUtil.get_constant_type(_theme_item)
+            var suffix := ThemeUtil.get_constant_type_suffix(constant_type)
+            _spin_box.visible = true
+            _spin_box.value = constant
+            _spin_box.suffix = suffix
+        Theme.DATA_TYPE_FONT:
+            var font: Font = value as Font
+            _resource_picker.visible = true
+            _resource_picker.base_type = "Font"
+            _resource_picker.edited_resource = font
+        Theme.DATA_TYPE_FONT_SIZE:
+            var font_size: int = value as int
+            _spin_box.visible = true
+            _spin_box.value = font_size
+            _spin_box.suffix = "pt"
+        Theme.DATA_TYPE_ICON:
+            var icon: Texture2D = value as Texture2D
+            _resource_picker.visible = true
+            _resource_picker.base_type = "Texture2D"
+            _resource_picker.edited_resource = icon
+        Theme.DATA_TYPE_STYLEBOX:
+            var style_box: StyleBox = value as StyleBox
+            _resource_picker.visible = true
+            _resource_picker.base_type = "StyleBox"
+            _resource_picker.edited_resource = style_box
             
+    
 func _draw_preview() -> void:
     var canvas_item := _preview.get_canvas_item()
     var canvas_item_rect := Rect2(Vector2.ZERO, _preview.size)
