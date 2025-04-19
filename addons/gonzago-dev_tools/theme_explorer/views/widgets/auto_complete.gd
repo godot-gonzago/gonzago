e21c2bc54b49ed4d62cfb1267dc9adcfc100356f
David Krummenacher <dave@kruda.ch>
2025-04-19 20:14:30 +0200

    Merp.

--- addons/gonzago-dev_tools/theme_explorer/views/widgets/auto_complete.gd
+++ addons/gonzago-dev_tools/theme_explorer/views/widgets/auto_complete.gd
@@ -40,6 +40,21 @@ func get_max_lines() -> int:
 func _notification(what: int) -> void:
     match what:
         NOTIFICATION_READY:
+            # Start with the popup not being able to grab focus
+            #unfocusable = true
+
+            # When the popup menu is about to popup make it focusable so we can control it with the mouse
+            # connect it in a deferred way so it does it at the end of the frame and does not steal the focus
+            #about_to_popup.connect(
+            #    func(): unfocusable = false,
+            #    CONNECT_DEFERRED
+            #)
+
+            # When the popup menu is going to hide make it unfocusable again
+            #popup_hide.connect(
+            #    func(): unfocusable = true
+            #)
+
             _recalculate_max_size()
         NOTIFICATION_THEME_CHANGED:
             if is_node_ready():
@@ -66,16 +81,15 @@ func _get_contents_minimum_size() -> Vector2:
 
 
 func _recalculate_max_size() -> void:
-    if is_inside_tree():
-        var panel_height := get_theme_stylebox("panel").get_minimum_size().y
-        var font := get_theme_font("font")
-        var font_size := get_theme_font_size("font_size")
-        var font_height := font.get_height(font_size)
-        var v_separation := get_theme_constant("v_separation")
-        var item_height := font_height + v_separation
-        var max_height := panel_height + item_height * max_lines
-        #max_height -= v_separation
-        max_size.y = max_height
+    var panel_height := get_theme_stylebox("panel").get_minimum_size().y
+    var font := get_theme_font("font")
+    var font_size := get_theme_font_size("font_size")
+    var font_height := font.get_height(font_size)
+    var v_separation := get_theme_constant("v_separation")
+    var item_height := font_height + v_separation
+    var max_height := panel_height + item_height * max_lines
+    #max_height -= v_separation
+    max_size.y = max_height
 
 
 func _focus_entered() -> void:
@@ -92,15 +106,30 @@ func _text_changed(new_text: String) -> void:
         min_size.y
     )
 
-    var window := line_edit.get_last_exclusive_window()
-    var screen := window.current_screen
-    var screen_rect := DisplayServer.screen_get_usable_rect(screen)
+    #var panel_height := get_theme_stylebox("panel").get_minimum_size().y
+    #var font := get_theme_font("font")
+    #var font_size := get_theme_font_size("font_size")
+    #var font_height := font.get_height(font_size)
+    #var v_separation := get_theme_constant("v_separation")
+    #var item_height := font_height + v_separation
+    #var max_height := panel_height + item_height * max_lines
+
+    #var window := line_edit.get_last_exclusive_window()
+    #var window_rect: Rect2
+    #if window.is_embedded():
+        #window_rect = window.get_visible_rect()
+    #else:
+        #var screen := window.current_screen
+        #window_rect = DisplayServer.screen_get_usable_rect(screen)
 
     #print(rect)
-    #print(screen_rect)
+    #print(window_rect)
 
-    #if screen_rect.end.y < rect.end.y:
-    #    max_size.y = screen_rect.end.y - rect.position.y
-    #    rect.end.y = screen_rect.end.y
+    #if window_rect.end.y < rect.end.y:
+        #rect.end.y = window_rect.end.y
+        #max_height = minf(max_height, rect.size.y)
 
-    popup(rect)
+    #max_size.y = max_height
+    #popup(rect)
+
+    popup_on_parent(rect)
