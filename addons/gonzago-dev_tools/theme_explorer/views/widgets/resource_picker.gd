69fb6797696bfa251759e3091ec4ba7005557915
David Krummenacher <dave@kruda.ch>
2025-04-28 21:28:00 +0200

    Merp.

--- addons/gonzago-dev_tools/theme_explorer/views/widgets/resource_picker.gd
+++ addons/gonzago-dev_tools/theme_explorer/views/widgets/resource_picker.gd
@@ -163,7 +163,7 @@ func _file_selected(path: String) -> void:
                 res_type = script_type
 
         for i in base_type.get_slice_count(","):
-            var base := base_type.get_slice(",", i)
+            var base := base_type.get_slicec(",".unicode_at(0), i)
             any_type_matches = ClassDB.is_parent_class(res_type, base) if is_global_class else loaded_resource.is_class(base)
             if any_type_matches:
                 break
@@ -263,7 +263,7 @@ func _update_menu_items() -> void:
         else:
             var res_type := _get_resource_type(cb)
             for i in base_type.get_slice_count(","):
-                var base := base_type.get_slicec(int(","), i)
+                var base := base_type.get_slicec(",".unicode_at(0), i)
                 paste_valid = ClassDB.is_parent_class(res_type, base)
                 if paste_valid:
                     break
@@ -293,43 +293,272 @@ func _update_menu_items() -> void:
 
 
 func _edit_menu_cbk(which: int) -> void:
-    pass
-# https://github.com/godotengine/godot/blob/master/editor/editor_resource_picker.cpp#L317
+    match which:
+        MenuOption.OBJ_MENU_LOAD:
+            var extensions: PackedStringArray = []
+            for i in base_type.get_slice_count(","):
+                var base := base_type.get_slicec(",".unicode_at(0), i)
+                extensions.append_array(ResourceLoader.get_recognized_extensions_for_type(base))
+                if ClassDB.class_exists(base):
+                    extensions.append_array(ResourceLoader.get_recognized_extensions_for_type(ClassDB.get_parent_class(base)))
+
+            var valid_extensions: Array[String] = []
+            valid_extensions.append_array(extensions)
+
+            if not _file_dialog:
+                _file_dialog = EditorFileDialog.new()
+                _file_dialog.file_mode = EditorFileDialog.FILE_MODE_OPEN_FILE
+                add_child(_file_dialog)
+                _file_dialog.file_selected.connect(_file_selected)
+
+            _file_dialog.clear_filters()
+            for e in valid_extensions:
+                _file_dialog.add_filter("*.%s" % e, e.to_upper())
+
+            _file_dialog.popup_file_dialog()
+        MenuOption.OBJ_MENU_QUICKLOAD:
+            var base_types_string := base_type.split(",")
+            var base_types: PackedStringArray = []
+            for type in base_types_string:
+                base_types.push_back(type)
+            if Engine.is_editor_hint():
+                EditorInterface.popup_quick_open(_file_selected, base_types)
+        MenuOption.OBJ_MENU_INSPECT:
+            if edited_resource:
+                resource_selected.emit(edited_resource, true)
+        MenuOption.OBJ_MENU_CLEAR:
+            edited_resource = null
+            _resource_changed()
+        MenuOption.OBJ_MENU_MAKE_UNIQUE:
+            if not edited_resource:
+                return
+            var unique_resource := edited_resource.duplicate()
+            if not unique_resource: # duplicate() may fail.
+                push_error("Failed to duplicate resource!")
+                return
+            edited_resource = unique_resource
+            _resource_changed()
+        MenuOption.OBJ_MENU_MAKE_UNIQUE_RECURSIVE:
+            if not edited_resource:
+                return
+            if not _duplicate_resources_dialog:
+                _duplicate_resources_dialog = ConfirmationDialog.new()
+                add_child(_duplicate_resources_dialog)
+                _duplicate_resources_dialog.title = tr("Make Unique (Recursive)")
+                _duplicate_resources_dialog.confirmed.connect(_duplicate_selected_resources)
+
+                var vb := VBoxContainer.new()
+                _duplicate_resources_dialog.add_child(vb)
+                var label := Label.new()
+                label.text = tr("Select resources to make unique:")
+                vb.add_child(label)
+
+                _duplicate_resources_tree = Tree.new()
+                #duplicate_resources_tree->set_accessibility_name(TTRC("Duplicate resources"));
+                _duplicate_resources_tree.auto_translate = AUTO_TRANSLATE_MODE_DISABLED
+                vb.add_child(_duplicate_resources_tree)
+                _duplicate_resources_tree.columns = 2
+                _duplicate_resources_tree.size_flags_vertical = Control.SIZE_EXPAND_FILL
+
+            _duplicate_resources_tree.clear()
+            var root := _duplicate_resources_tree.create_item()
+            _gather_resources_to_duplicate(edited_resource, root)
+
+            _duplicate_resources_dialog.reset_size()
+            _duplicate_resources_dialog.popup_centered(Vector2(500, 400) * EditorInterface.get_editor_scale())
+        MenuOption.OBJ_MENU_SAVE:
+            if not edited_resource:
+                return
+            #EditorNode::get_singleton()->save_resource(edited_resource);
+        MenuOption.OBJ_MENU_SAVE_AS:
+            if not edited_resource:
+                return
+            var resource_saved := _resource_saved
+            #if (!EditorNode::get_singleton()->is_connected("resource_saved", resource_saved)) {
+            #    EditorNode::get_singleton()->connect("resource_saved", resource_saved);
+            #}
+            #EditorNode::get_singleton()->save_resource_as(edited_resource);
+        MenuOption.OBJ_MENU_COPY:
+            #EditorSettings::get_singleton()->set_resource_clipboard(edited_resource);
+            pass
+        MenuOption.OBJ_MENU_PASTE:
+            #edited_resource = EditorSettings::get_singleton()->get_resource_clipboard();
+            #if edited_resource.is_built_in() and EditorInterface.get_edited_scene_root() and \
+            #    edited_resource.resource_path.get_slice("::", 0) != EditorInterface.get_edited_scene()->get_scene_file_path():
+            #    # Automatically make resource unique if it belongs to another scene.
+            #    _edit_menu_cbk(MenuOption.OBJ_MENU_MAKE_UNIQUE)
+            #    return
+            #_resource_changed()
+            pass
+        MenuOption.OBJ_MENU_SHOW_IN_FILE_SYSTEM:
+            EditorInterface.get_file_system_dock().navigate_to_path(edited_resource.resource_path)
+        _:
+            # Allow subclasses to handle their own options first, only then fallback on the default branch logic.
+            if handle_menu_selected(which):
+                return
+
+            if which >= MenuOption.CONVERT_BASE_ID:
+                var to_type := which - MenuOption.CONVERT_BASE_ID
+                var conversions: Array[EditorResourceConversionPlugin] = [] # EditorNode::get_singleton()->find_resource_conversion_plugin_for_resource(edited_resource);
+                if to_type <= conversions.size():
+                    push_error("Index out of range for EditorResourceConversionPlugin.")
+                    return
+
+                #edited_resource = conversions[to_type].convert(edited_resource)
+                _resource_changed()
+
+            if _inheritors_array.is_empty():
+                push_error("No inheritors!")
+                return
+
+            var intype = _inheritors_array[which - MenuOption.TYPE_BASE_ID]
+            var obj: Variant
+
+            if ClassDB.is_class_enabled(intype):
+                obj = ClassDB.instantiate(intype) # EditorNode::get_editor_data().script_class_instance(intype);
+            else:
+                obj = ClassDB.instantiate(intype)
+
+            if not obj:
+                obj = ClassDB.instantiate(intype) # EditorNode::get_editor_data().instantiate_custom_type(intype, "Resource");
+
+            var resp: Resource = null
+            #Resource *resp = Object::cast_to<Resource>(obj);
+            if not resp:
+                push_error("Failed to convert resource!")
+                return
+
+            #EditorNode::get_editor_data().instantiate_object_properties(obj);
+
+            # Prevent freeing of the object until the end of the update of the resource (GH-88286).
+            var old_edited_resource := edited_resource
+            edited_resource = resp
+            _resource_changed()
 
 
 func set_create_options(menu_node: Object) -> void:
-    pass
-# https://github.com/godotengine/godot/blob/master/editor/editor_resource_picker.cpp#L497
+    _ensure_resource_menu()
+    # If a subclass implements this method, use it to replace all create items.
+    if menu_node.call_v(&"_set_create_options"):
+        return
+
+    # By default provide generic "New ..." options.
+    if not base_type.is_empty():
+        var idx := 0
+        _ensure_allowed_types()
+        var allowed_types := _allowed_types_without_convert
+        if not allowed_types.is_empty():
+            _edit_menu.add_separator(tr("New"))
+        for e in allowed_types:
+            if not ClassDB.can_instantiate(e):
+                continue
+            _inheritors_array.push_back(e)
+            var icon := ThemeDB.fallback_icon # EditorNode::get_singleton()->get_class_icon(t, "Object");
+            var id := MenuOption.TYPE_BASE_ID + idx
+            _edit_menu.add_icon_item(icon, e, id)
+            #edit_menu->set_item_auto_translate_mode(-1, AUTO_TRANSLATE_MODE_DISABLED);
+            #HashMap<String, DocData::ClassDoc>::Iterator class_doc = EditorHelp::get_doc_data()->class_list.find(t);
+            #if (class_doc) {
+            #    edit_menu->set_item_tooltip(-1, DTR(class_doc->value.brief_description));
+            #}
+            idx += 1
+        if _edit_menu.item_count:
+            _edit_menu.add_separator()
 
 
 func handle_menu_selected(which: int) -> bool:
-    return false
-# https://github.com/godotengine/godot/blob/master/editor/editor_resource_picker.cpp#L542
+    return callv(&"_handle_menu_selected", [which])
 
 
 func _button_draw() -> void:
-    pass
-# https://github.com/godotengine/godot/blob/master/editor/editor_resource_picker.cpp#L548
+    if _dropping:
+        var color := get_theme_color(&"accent_color", &"Editor")
+        _assign_button.draw_rect(Rect2(Vector2.ZERO, _assign_button.size), color, false)
 
 
 func _button_input(event: InputEvent) -> void:
-    pass
-# https://github.com/godotengine/godot/blob/master/editor/editor_resource_picker.cpp#L555
+    var mb := event as InputEventMouseButton
+    if mb and mb.is_pressed() and mb.button_index == MOUSE_BUTTON_RIGHT:
+        # Only attempt to update and show the menu if we have
+        # a valid resource or the Picker is editable, as
+        # there will otherwise be nothing to display.
+        if edited_resource or is_editable():
+            if _edit_menu and _edit_menu.visible:
+                _edit_button.set_pressed_no_signal(false)
+                _edit_menu.hide()
+                return
+
+            _update_menu_items()
+
+            var pos := get_screen_position() + mb.position
+            _edit_menu.reset_size()
+            _edit_menu.position = pos
+            _edit_menu.popup()
 
 
 func _get_resource_type(resource: Resource) -> String:
-    return ""
-# https://github.com/godotengine/godot/blob/master/editor/editor_resource_picker.cpp#L579
+    if not resource:
+        return ""
+    var res_type := resource.get_class()
+    var res_script := resource.get_script() as Script
+    if not res_script:
+        return res_type
+
+    # TODO: Replace with EditorFileSystem when PR #60606 is merged to use cached resource type.
+    var script_type := res_script.get_global_name()
+    if not script_type.is_empty():
+        res_type = script_type
+
+    return res_type
 
 
 static func _add_allowed_types(type: StringName, vector: Array[StringName]) -> void:
-    pass
-# https://github.com/godotengine/godot/blob/master/editor/editor_resource_picker.cpp#L598
+    if vector.find(type):
+        # Already added.
+        return
+
+    if ClassDB.class_exists(type):
+        # Engine class
+        if ClassDB.can_instantiate(type):
+            vector.push_back(type)
+        var inheriters := ClassDB.get_inheriters_from_class(type)
+        for s in inheriters:
+            _add_allowed_types(s, vector)
+    else:
+        # Script class
+        vector.push_back(type)
+
+    #List<StringName> inheriters;
+    #ScriptServer::get_inheriters_list(p_type, &inheriters);
+    #for (const StringName &S : inheriters) {
+    #    _add_allowed_type(S, p_vector);
+    #}
 
 
 func _ensure_allowed_types() -> void:
-    pass
-# https://github.com/godotengine/godot/blob/master/editor/editor_resource_picker.cpp#L628
+    if not _allowed_types_without_convert.is_empty():
+        return
+    var final_allowed: Array[StringName] = []
+    var allowed_types: PackedStringArray = base_type.split(",")
+    var size := allowed_types.size()
+    for s in allowed_types:
+        var base := s.strip_edges()
+        if base.begins_with("-"):
+            final_allowed.erase(base.right(-1))
+            continue
+        _add_allowed_types(base, final_allowed)
+
+    for s in final_allowed:
+        _allowed_types_without_convert.append(s)
+    _allowed_types_with_convert = Array(_allowed_types_without_convert)
+    for i in size:
+        var base := allowed_types[i].strip_edges()
+        if base == "BaseMaterial3D":
+            _allowed_types_with_convert.append("Texture2D")
+        elif ClassDB.is_parent_class("ShaderMaterial", base):
+            _allowed_types_with_convert.append("Shader")
+        elif ClassDB.is_parent_class("ImageTexture", base):
+            _allowed_types_with_convert.append("Image")
 
 
 func _is_drop_valid(drag_data: Dictionary) -> bool:
